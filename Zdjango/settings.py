be72454e470dc8bf881d46bef99e579be1a979da
KDS-Limitless <limitlesskds@gmail.com>
2021-04-29 16:01:14 +0100

    ...

--- Zdjango/settings.py
+++ Zdjango/settings.py
@@ -141,13 +141,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = "587"
 EMAIL_USE_TLS = True
-<<<<<<< HEAD
 EMAIL_HOST_USER = os.environ.get("Email")
 EMAIL_HOST_PASSWORD = os.environ.get("password")
-=======
-EMAIL_HOST_USER = "s.kamahjnr@gmail.com"
-EMAIL_HOST_PASSWORD = "*c*0*30#spideros##"
->>>>>>> 3711a4a54eabf493403f69841a31260d349b68a5
 DEFAULT_FROM_EMAIL = "XIblog"
 
 db_from_env = dj_database_url.config(conn_max_age=500)
