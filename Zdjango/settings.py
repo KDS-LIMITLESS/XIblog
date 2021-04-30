9b67aa10abb5fc97ce8d4a0d5fe6ea1e55c8341c
KDS-Limitless <limitlesskds@gmail.com>
2021-04-30 12:33:13 +0100

    Project Settings

--- Zdjango/settings.py
+++ Zdjango/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -137,12 +137,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = "index"
 LOGIN_URL = "login/"
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = "587"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("Email")
-EMAIL_HOST_PASSWORD = os.environ.get("password")
+EMAIL_HOST_USER = os.environ.get('Email')
+EMAIL_HOST_PASSWORD = os.environ.get('password')
 DEFAULT_FROM_EMAIL = "XIblog"
 
 db_from_env = dj_database_url.config(conn_max_age=500)
